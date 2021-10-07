7b89fa1145b019d8e1a57b3e251a6769f0d2d3d6
Ondrej Nantl <xnantl01@vutbr.cz>
2021-10-07 13:32:34 +0200

    Creating a fcn Cokolada

---
+++ Cokolada.R
@@ -0,0 +1,11 @@
+Cokolada <- function(M,r,s){
+  if (r == nrow(M)){
+    return(M[r,s])
+  } else {
+    C <- M[r,s]
+    Cdolu <- Cokolada(M,r+1,s)
+    Csikmo <- Cokolada(M,r+1,s+1)
+    return (max(c(Cdolu, Csikmo)) + C)
+  }
+    
+}
